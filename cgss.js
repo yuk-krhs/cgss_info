c500d2c4df919e261e9827b78cd75bbc962ff250
yuk-krhs <yuk.krhs@gmail.com>
2022-11-11 20:09:44 +0900

    implement grayout card

--- cgss.js
+++ cgss.js
@@ -1,33 +1,96 @@
-function clickLink()
-{
-//	console.log('click');
+const grayCards = new Set();
+
+function getCookieMap() {
+	var	map	= new Map();
+
+//	console.log('get cookies');
+//	console.log(document.cookie);
+
+	try
+	{
+		for(var i of document.cookie.split(';'))
+		{
+	    	var kv	= i.split('=');
+
+			map.set(kv[0], kv[1]);
+		}
+	} catch(e) {}
+
+	return map;
+}
+
+function doClickLink(elem, imd_id) {
+	var	img	= document.getElementById(imd_id);
+
+	if(img.classList.contains('g'))
+	{
+		img.classList.remove('g');
+		grayCards.delete(imd_id);
+	} else
+	{
+		img.classList.add('g');
+		grayCards.add(imd_id);
+	}
+
+	document.cookie = "grayCards=" + Array.from(grayCards).join('/') + ";max-age=" + (24*60*60*365*10);
+
+//	console.log('set cookies');
+//	console.log(document.cookie);
+
 	return false;
 }
 
-window.onmousemove = function (e) {
-	var x = e.x;
-	var y = e.y;
-	var	cw= window.innerWidth;
-	var	ch= window.innerHeight;
-	var element = document.elementFromPoint(x, y);
+function doDOMContentLoaded() {
+	var	cmap= getCookieMap();
+
+//	console.log(cmap);
+
+	if(cmap.has('grayCards'))
+	{
+		var	cards	= cmap.get('grayCards');
+		var	list	= cards.split('/');
+
+		for(var i of list)
+		{
+			var	elem= document.getElementById(i);
+
+			if(elem)
+			{
+				elem.classList.add('g');
+				grayCards.add(i);
+			}
+		}
+	}
+}
+
+function doOnMouseMove(e) {
+	var x		= e.x;
+	var y		= e.y;
+	var	cw		= window.innerWidth;
+	var	ch		= window.innerHeight;
+	var elem	= document.elementFromPoint(x, y);
 	var	frame	= document.getElementById('cardinfo-frame');
 
-	if(element && element.tagName == 'IMG')
+	if(elem && elem.tagName == 'IMG')
 	{
-		element	= element.parentElement;
-	  	//console.log(element);
+		var	img	= elem;
+		var	id	= img.getAttribute('ID');
+		elem	= elem.parentElement;
 
-		if(element && element.tagName == 'SPAN')
+	//	console.log(elem);
+
+		if(elem && elem.tagName == 'SPAN')
 		{
-			element	= element.parentElement;
-	  		//console.log(element);
+			elem	= elem.parentElement;
 
-			if(element && element.tagName == 'A')
+	  	//	console.log(elem);
+
+			if(elem && elem.tagName == 'A')
 			{
 				var	ww		= 540;
 				var	wh		= 260;
 				var	iframe	= document.getElementById('cardinfo');
-				var	href	= element.getAttribute('HREF');
+				var	href	= elem.getAttribute('HREF');
 
 				//if(frame.classList.contains('show-popup')
 				//&& iframe.getAttribute('SRC') ==  href)
@@ -57,3 +120,7 @@ window.onmousemove = function (e) {
 		frame.classList.add('hide-popup');
 	}
 }
+
+function clickLink(elem, imd_id) 							{ try { return doClickLink(elem, imd_id); }	catch(ex) { console.log(ex); return false; } }
+window.onmousemove = function (e)							{ try { doOnMouseMove(e); }					catch(ex) { console.log(ex); } }
+document.addEventListener('DOMContentLoaded', function()	{ try { doDOMContentLoaded(); }				catch(ex) { console.log(ex); } }, false);
